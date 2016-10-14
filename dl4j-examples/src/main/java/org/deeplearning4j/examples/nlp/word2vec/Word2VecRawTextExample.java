8265bd9284fc4e67c3e3c934f2efb10f476cb36f
Gil Noh <noh@omq.de>
2016-10-14 10:03:49 +0200

    testing to generate brille24 data

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/nlp/word2vec/Word2VecRawTextExample.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/nlp/word2vec/Word2VecRawTextExample.java
@@ -1,7 +1,12 @@
 package org.deeplearning4j.examples.nlp.word2vec;
 
 import org.datavec.api.util.ClassPathResource;
+import org.deeplearning4j.models.embeddings.learning.impl.elements.CBOW;
+import org.deeplearning4j.models.embeddings.learning.impl.elements.SkipGram;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
+import org.deeplearning4j.models.glove.Glove;
+import org.deeplearning4j.models.word2vec.VocabWord;
 import org.deeplearning4j.models.word2vec.Word2Vec;
 import org.deeplearning4j.text.sentenceiterator.BasicLineIterator;
 import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
@@ -12,6 +17,7 @@ import org.deeplearning4j.ui.UiServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.util.Collection;
 
 /**
@@ -19,11 +25,13 @@ import java.util.Collection;
  */
 public class Word2VecRawTextExample {
 
+    // Modified to generate Brille24 data.
     private static Logger log = LoggerFactory.getLogger(Word2VecRawTextExample.class);
 
     public static void main(String[] args) throws Exception {
 
-        String filePath = new ClassPathResource("raw_sentences.txt").getFile().getAbsolutePath();
+//        String filePath = new ClassPathResource("raw_sentences.txt").getFile().getAbsolutePath();
+        String filePath = "/home/tailblues/omq/brille24_11krequest.txt";
 
         log.info("Load & Vectorize Sentences....");
         // Strip white space before and after for each line
@@ -34,8 +42,11 @@ public class Word2VecRawTextExample {
 
         log.info("Building model....");
         Word2Vec vec = new Word2Vec.Builder()
+                .elementsLearningAlgorithm(new SkipGram<VocabWord>())
+//                .elementsLearningAlgorithm(new CBOW<VocabWord>())
                 .minWordFrequency(5)
                 .iterations(1)
+                .epochs(10)
                 .layerSize(100)
                 .seed(42)
                 .windowSize(5)
@@ -49,12 +60,24 @@ public class Word2VecRawTextExample {
         log.info("Writing word vectors to text file....");
 
         // Write word vectors
-        WordVectorSerializer.writeWordVectors(vec, "pathToWriteto.txt");
+        WordVectorSerializer.writeWordVectors(vec, "Brille24Vectors.txt");
 
-        log.info("Closest Words:");
-        Collection<String> lst = vec.wordsNearest("day", 10);
-        System.out.println(lst);
-        UiServer server = UiServer.getInstance();
-        System.out.println("Started on port " + server.getPort());
+//        WordVectors vec = WordVectorSerializer.loadTxtVectors(new File("Brille24Vectors_300dim.txt"));
+//        WordVectors vec = WordVectorSerializer.loadTxtVectors(new File("/home/tailblues/Dropbox/Brille24Vectors.txt"));
+
+        System.out.println(vec.wordsNearest("brille", 20));
+        System.out.println(vec.wordsNearest("paypal", 20));
+        System.out.println(vec.wordsNearest("versand", 20));
+        System.out.println(vec.wordsNearest("sendung", 20));
+        System.out.println(vec.wordsNearest("lieferung", 20));
+        System.out.println(vec.wordsNearest("juni", 20));
+
+        System.out.println(vec.similarity("paypal", "bezahlung"));
+        System.out.println(vec.similarity("lieferung", "sendung"));
+        System.out.println(vec.similarity("paket", "lieferung"));
+        System.out.println(vec.similarity("paket", "versand"));
+
+//        UiServer server = UiServer.getInstance();
+//        System.out.println("Started on port " + server.getPort());
     }
 }
